36e4cdab56c57d8bfb4de41adc324b629720c84c
RaniSales <Ranievly5@hotmail.com>
2022-05-04 16:39:19 -0300

    Footer-simples

--- pokedex/src/pages/Home.jsx
+++ pokedex/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../Components/header/Header"
+import Footer from "../Components/footer/Footer"
 
 export function Home(){
     return(
         <div>
             <Header/>
             <p>Home</p>
+
+            <Footer/>
         </div>
     )
 }
